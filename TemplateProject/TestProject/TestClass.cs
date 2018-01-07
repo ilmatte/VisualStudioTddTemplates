0158de6422d1546dffd6c6bd947d4345471bb1f0
ilmatte <m.tontini@gmail.com>
2018-01-07 22:37:26 +0100

    template project commit

---
+++ TemplateProject/TestProject/TestClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace $safeprojectname$
+{
+    [TestFixture]
+    public class TestClass
+    {
+        MockRepository mocks;
+
+        /// <summary>
+        /// Prepares mock repository
+        /// </summary>
+        [SetUp]
+        public void Initialize()
+        {
+            mocks = new MockRepository();
+        }
+
+        /// <summary>
+        /// template behavior and state testing method
+        /// </summary>
+        [Test]
+        public void TestMethod1()
+        {
+            IDependency dependency = mocks.CreateMock<IDependency>();
+
+            // Record expectations
+            using (mocks.Record())
+            {
+                Expect.Call(dependency.Method1("parameter")).Return("result");
+                dependency.Method2();
+            }
+
+            // Replay and validate interaction
+            Subject subjectUnderTest;
+            using (mocks.Playback())
+            {
+                subjectUnderTest = new Subject(dependency);
+                subjectUnderTest.DoWork();
+            }
+
+            // Post-interaction assertion
+            Assert.That(subjectUnderTest.WorkDone, Is.True);
+        }
+    }
+ }
